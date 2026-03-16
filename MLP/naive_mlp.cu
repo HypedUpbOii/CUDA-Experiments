70609e3b2c858dc95ed49dfdb8cd66692eef944d
Sandeep Reddy Nallamilli <sandeep.reddy.nallamilli2005@gmail.com>
2026-03-16 18:12:54 +0530

    added time

--- MLP/naive_mlp.cu
+++ MLP/naive_mlp.cu
@@ -28,14 +28,17 @@ __global__ void relu(float* B, int M, int N) {
 
 int main(int argc, char* argv[]) {
     int B, N;
+    bool perform_check = false;
     B = std::atoi(argv[1]);
     N = std::atoi(argv[2]);
+    if (argc > 3) {
+        perform_check = true;
+    }
 
     float* inputs = new float[B * N];
     float* weights1 = new float[N * N];
     float* weights2 = new float[N * N];
     float* answer = new float[B * N];
-    float* answer_ref = new float[B * N];
 
     // random number generator
     std::random_device rd;
@@ -65,9 +68,19 @@ int main(int argc, char* argv[]) {
     cudaMemcpy(I, inputs, sizeof(float) * B * N, cudaMemcpyHostToDevice);
     cudaMemcpy(W1, weights1, sizeof(float) * N * N, cudaMemcpyHostToDevice);
     cudaMemcpy(W2, weights2, sizeof(float) * N * N, cudaMemcpyHostToDevice);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start);
     matmul<<<blocksPerGrid, threadsPerBlock>>>(I, W1, res, B, N, N);
     relu<<<blocksPerGrid, threadsPerBlock>>>(res, B, N);
     matmul<<<blocksPerGrid, threadsPerBlock>>>(res, W2, I, B, N, N);
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+
+    float ms;
+    cudaEventElapsedTime(&ms, start, stop);
 
     cudaMemcpy(answer, I, sizeof(float) * B * N, cudaMemcpyDeviceToHost);
     cudaFree(I);
@@ -75,49 +88,54 @@ int main(int argc, char* argv[]) {
     cudaFree(W2);
     cudaFree(res);
 
-    float* hidden = new float[B * N];
-
-    // hidden = inputs * weights1
-    for (int y = 0; y < B; ++y) {
-        for (int x = 0; x < N; ++x) {
-            float sum = 0.0f;
-            for (int k = 0; k < N; ++k) {
-                sum += inputs[y * N + k] * weights1[k * N + x];
+    if (perform_check) {
+        float* hidden = new float[B * N];
+        float* answer_ref = new float[B * N];
+    
+        // hidden = inputs * weights1
+        for (int y = 0; y < B; ++y) {
+            for (int x = 0; x < N; ++x) {
+                float sum = 0.0f;
+                for (int k = 0; k < N; ++k) {
+                    sum += inputs[y * N + k] * weights1[k * N + x];
+                }
+                hidden[y * N + x] = sum;
             }
-            hidden[y * N + x] = sum;
         }
-    }
-
-    // ReLU
-    for (int i = 0; i < B * N; ++i) {
-        if (hidden[i] < 0.0f) hidden[i] = 0.0f;
-    }
-
-    // answer_ref = hidden * weights2
-    for (int y = 0; y < B; ++y) {
-        for (int x = 0; x < N; ++x) {
-            float sum = 0.0f;
-            for (int k = 0; k < N; ++k) {
-                sum += hidden[y * N + k] * weights2[k * N + x];
+    
+        // ReLU
+        for (int i = 0; i < B * N; ++i) {
+            if (hidden[i] < 0.0f) hidden[i] = 0.0f;
+        }
+    
+        // answer_ref = hidden * weights2
+        for (int y = 0; y < B; ++y) {
+            for (int x = 0; x < N; ++x) {
+                float sum = 0.0f;
+                for (int k = 0; k < N; ++k) {
+                    sum += hidden[y * N + k] * weights2[k * N + x];
+                }
+                answer_ref[y * N + x] = sum;
             }
-            answer_ref[y * N + x] = sum;
         }
+    
+        float max_diff = 0.0f;
+    
+        for (int i = 0; i < B * N; ++i) {
+            float diff = std::abs(answer[i] - answer_ref[i]);
+            if (diff > max_diff) max_diff = diff;
+        }
+    
+        std::cout << "Max difference: " << max_diff << std::endl;
+        delete[] answer_ref;
+        delete[] hidden;
     }
 
-    float max_diff = 0.0f;
-
-    for (int i = 0; i < B * N; ++i) {
-        float diff = std::abs(answer[i] - answer_ref[i]);
-        if (diff > max_diff) max_diff = diff;
-    }
-
-    std::cout << "Max difference: " << max_diff << std::endl;
+    std::cout << "Time elapsed: " << ms << std::endl;
 
     delete[] inputs;
     delete[] weights1;
     delete[] weights2;
     delete[] answer;
-    delete[] answer_ref;
-    delete[] hidden;
     return 0;
 }
